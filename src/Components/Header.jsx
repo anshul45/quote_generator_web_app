4c8e5a4833c27470eca376e51a83b54f17969b47
Anshul45 <anshulgarwal025@gmail.com>
2023-11-08 20:12:16 +0530

    created header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>
+    <div className='flex justify-between py-5 px-14 bg-blue-700 text-white'>
       <div>Home</div>
       <div>Bookmarks</div>
     </div>
