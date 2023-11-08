0b612dcebd2ed1d18169b915ab282c6f60870fd3
Anshul45 <anshulgarwal025@gmail.com>
2023-11-08 20:06:51 +0530

    added tailwind in projects for fast development

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+      <div>Home</div>
+      <div>Bookmarks</div>
+    </div>
+  )
+}
+
+export default Header
